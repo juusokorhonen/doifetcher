da419d1b848a3593ec918fbb6c38c0fc7baf5d9d
Juuso Korhonen <jk.github@turqoosi.net>
2014-10-06 11:16:24 +0300

    Started admin interface.

--- doifetcher/admin/__init__.py
+++ doifetcher/admin/__init__.py
@@ -8,17 +8,43 @@ from doifetcher.model import Article, Author, Journal
 
 admin_section = Admin()
 
-class AdminView(BaseView):
+class _AdminView(BaseView):
     @expose('/')
     def index(self):
         return self.render('admin/index.html')
 
 # Test view
-admin_section.add_view(AdminView('Hello'))
+admin_section.add_view(_AdminView('Hello'))
+
+class _DbView(ModelView):
+    can_create = False
+    
+    # Excluded columns
+    column_exclude_list = ['mod_date', 'add_date']
+
+    def __init__(self, model, session, **kwargs):
+        super(_DbView, self).__init__(model, session, **kwargs)
+        # Override certain columns
+        #column_list = self.get_list_columns()
+        #print(column_list)
+        #self.column_list = [key for (key,value) in column_list if (key not in self.excluded_columns)]
+        #print(self.column_list)
+        print(self.get_list_columns())
+
+class _ArticleDbView(_DbView):
+    pass
+
+class _AuthorDbView(_DbView):
+    pass
+
+class _JournalDbView(_DbView):
+    pass
 
 import types
 # Initialize database connection
 def init_db(self, db):
-    self.add_view(ModelView(Journal, db.session))
+    self.add_view(_ArticleDbView(Article, db.session))
+    self.add_view(_AuthorDbView(Author, db.session))
+    self.add_view(_JournalDbView(Journal, db.session))
 
 admin_section.init_db = types.MethodType(init_db, admin_section)
