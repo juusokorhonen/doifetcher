79a24cb98c83199c8db63fe25fc36f6627723744
Juuso Korhonen <jk.github@turqoosi.net>
2015-03-31 09:43:40 +0300

    Migrated to use Flask-script

--- runserver.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from doifetcher import create_app
-from flask import url_for
-from config import Config, DevelopmentConfig, TestingConfig, ProductionConfig
-
-app = create_app(config=DevelopmentConfig)
-app.run()
